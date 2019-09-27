ee1d7d1951375f9b9b3c7923b64c24529f9908dd
tylerccaceres <tyler.caceres@gmail.com>
2019-09-27 15:36:17 -0400

    functions created , arrows only - the juiciest and freshest

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
 
 const App = props => {
-  const [state] = useState({ selectedCharacter: 1, side: "light", destroyed: false });
+  const [state, setState] = useState({ selectedCharacter: 1, side: "light", destroyed: false });
+  const sideHandler = side => {
+    setState({});
+  };
 
+  const charSelectHandler = event => {
+    const charId = event.target.value;
+    setState({});
+  };
+
+  const destructionHandler = () => {
+    setState();
+  };
   return <div></div>;
 };
 
